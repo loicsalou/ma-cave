e0edb03aa651224ebb35ab001fd3de89f51a633b
Loic Salou <business.salou@gmail.com>
2017-03-12 15:37:48 +0100

    - CSS distribution - events distribution + gestion ouverture / fermeture distribution - essai native FilePath ne donne rien

--- src/components/distribution/distribution.ts
+++ src/components/distribution/distribution.ts
@@ -1,33 +1,46 @@
-import {Component, Input, EventEmitter} from '@angular/core';
+import {Component, Input, EventEmitter, OnChanges} from "@angular/core";
 
 /*
-  Generated class for the Distribution component.
+ Generated class for the Distribution component.
 
-  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
-  for more info on Angular 2 Components.
-*/
+ See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ for more info on Angular 2 Components.
+ */
 @Component({
   selector: 'distribution',
   templateUrl: 'distribution.html',
   styleUrls: ['/src/components/distribution/distribution.scss']
 })
-export class DistributionComponent {
+export class DistributionComponent implements OnChanges {
 
   @Input()
   distribution;
-  badgeClicked: EventEmitter<string> = new EventEmitter();
+  badgeClicked: EventEmitter<any> = new EventEmitter();
+  isOpened = false;
 
   constructor() {
   }
 
-  valueClicked($event) {
-    console.info("filter clicked: "+$event.currentTarget.textContent);
-    let value=$event.currentTarget.textContent.split(':')[0];
+  ngOnChanges() {
+    this.open();
+  }
+
+  open() {
+    this.isOpened=true;
+  }
+
+  close() {
+    this.isOpened=false;
+  }
+
+  valueClicked($event, axis: string) {
+    console.info('filter clicked: ' + $event.currentTarget.textContent + ' axis is ' + axis);
+    let value = {value: $event.currentTarget.textContent.split(':')[0], axis: axis};
     this.badgeClicked.emit(value);
+    this.isOpened=false;
   }
 
 }
-
 
 
 // WEBPACK FOOTER //
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../../node_modules/ionic-angular/navigation/nav-controller.d.ts"/>
 import {Component, OnInit} from "@angular/core";
 import {NavController} from "ionic-angular";
-import {Camera, ImagePicker} from "ionic-native";
+import {Camera, ImagePicker, FilePath} from "ionic-native";
 
 @Component({
   selector: 'page-about',
@@ -28,13 +28,20 @@ export class AboutPage implements OnInit {
 
   onclick(event: any) {
     console.info('zone ' + event.currentTarget.title + ' cliquée');
-    let options = { destinationType: Camera.DestinationType.DATA_URL, encodingType: Camera.EncodingType.JPEG, mediaType: Camera.MediaType.PICTURE, sourceType: 1, saveToPhotoAlbum: false, correctOrientation: true };
+    let options = {
+      destinationType: Camera.DestinationType.DATA_URL,
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      sourceType: 1,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
     Camera.getPicture(options).then((imageData) => {
       let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.message='image capturée !';
+      this.message = 'image capturée !';
     }, (err) => {
       console.error('Erreur lors de la prise de la photo !');
-      this.message='Erreur lors de la prise de la photo ! '+err;
+      this.message = 'Erreur lors de la prise de la photo ! ' + err;
     });
   }
 
@@ -54,16 +61,25 @@ export class AboutPage implements OnInit {
      */
     ImagePicker.getPictures({maximumImagesCount: 3}).then((results) => {
       for (var i = 0; i < results.length; i++) {
-        this.imagePath='data:image/jpeg;base64,' + results[i];
-        this.message=this.imagePath
-      }
-    }, (err) => {
-      this.message = "Erreur d'accès à la photo ! " + err;
-    });
+        FilePath.resolveNativePath(results[i])
+          .then(filePath => {
+            console.log("Chemin résolu " + filePath);
+            this.message = this.imagePath;
+          })
+          .catch(err => this.message = err);
   }
+}
+,
+(err) => {
+  this.message = "Erreur d'accès à la photo ! " + err;
+}
+)
+;
+}
 
-  removePicture(event) {
-    event.stopPropagation();
-    this.imagePath=null;
-  }
+removePicture(event)
+{
+  event.stopPropagation();
+  this.imagePath = null;
+}
 }
