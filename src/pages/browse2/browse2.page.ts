fedb857ebe5038d65d21876fc9c52be678df4b07
Loic Salou <business.salou@gmail.com>
2017-03-12 17:42:48 +0100

    - filtrage fonctionne mais pas encore satisfaisant

--- src/pages/browse2/browse2.page.ts
+++ src/pages/browse2/browse2.page.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {Platform, NavController, ActionSheetController, ToastController} from "ionic-angular";
+import {Platform, NavController, ToastController} from "ionic-angular";
 import {BottleService} from "../../components/bottle/bottle.service";
 import {DistributeService} from "../../components/distribution/distribute.service";
 
@@ -12,28 +12,51 @@ export class Browse2Page {
 
   bottles;
   distribution; //distribution de la sélection selon plusieurs colonnes pour avoir le compte
+  isDistributionOpened = false;
 
   constructor(private toastCtrl: ToastController, public navCtrl: NavController, public platform: Platform,
               private bottlesService: BottleService, private distributionService: DistributeService) {
-    this.bottles=bottlesService.getBottles();
+    this.bottles = bottlesService.getBottles();
   }
 
-  filterOn(event: any) {
-    let filter=event.target.value
-    this.bottles=this.bottlesService.getBottlesByKeywords([filter]);
-    this.distribution=this.distributionService.distributeBy(this.bottles,['label', 'subregion_label', 'area_label', 'millesime']);
+  switchDistribution() {
+    this.isDistributionOpened = !this.isDistributionOpened;
+  }
+
+  openDistribution() {
+    this.isDistributionOpened = true;
+  }
+
+  closeDistribution() {
+    this.isDistributionOpened = false;
+  }
+
+  filterOnText(event: any) {
+    let filter = event.target.value
+    this.bottles = this.bottlesService.getBottlesByKeywords([filter]);
+    this.distribute(['label', 'subregion_label', 'millesime']);
     //this.presentToast();
   }
 
-  refine($event) {
-    let by='label';
-    let value=$event;
-    this.bottles=this.bottlesService.getBottlesBy(this.bottles, by, value);
+  refineFilter(jsonFilter: any) {
+    let by = jsonFilter.axis;
+    let value = jsonFilter.value;
+    this.bottles = this.bottlesService.getBottlesBy(this.bottles, by, value);
+    if (by==='subregion_label') {
+      this.distribute(['label', 'area_label', 'millesime'])
+    } else {
+      this.distribute(['label', 'subregion_label', 'millesime']);
+    }
   }
+
+  distribute(axis: string[]) {
+    this.distribution = this.distributionService.distributeBy(this.bottles, axis);
+  }
+
 
   presentToast() {
     let toast = this.toastCtrl.create({
-      message: (this.bottles && this.bottles.length > 0) ? this.bottles.length+' bouteilles trouvées' : 'Aucune bouteille ne correspond',
+      message: (this.bottles && this.bottles.length > 0) ? this.bottles.length + ' bouteilles trouvées' : 'Aucune bouteille ne correspond',
       duration: 3000,
       position: 'bottom',
       showCloseButton: true
