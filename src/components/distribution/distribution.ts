73fa9f35958322a51d869a7743183cfcffb2c248
Loic Salou <business.salou@gmail.com>
2017-03-16 00:09:29 +0100

    - qq ajustements CSS dans le détail des bouteilles - Ajout du nombre de bouteilles dans la distribution - rectification de l’index des slides (détail des bouteilles) il part de 1 et non pas de 0

--- src/components/distribution/distribution.ts
+++ src/components/distribution/distribution.ts
@@ -7,38 +7,38 @@ import {Component, Input, EventEmitter, OnChanges, Output} from "@angular/core";
  for more info on Angular 2 Components.
  */
 @Component({
-  selector: 'distribution',
-  templateUrl: 'distribution.html',
-  styleUrls: ['/src/components/distribution/distribution.scss']
-})
+             selector: 'distribution',
+             templateUrl: 'distribution.html',
+             styleUrls: [ '/src/components/distribution/distribution.scss' ]
+           })
 export class DistributionComponent implements OnChanges {
 
   @Input()
   distribution;
   @Input()
   opened = false;
+  @Input()
+  count: number = 0;
   @Output()
   badgeClicked: EventEmitter<any> = new EventEmitter();
-
 
   constructor() {
   }
 
   ngOnChanges() {
-    console.info("opened:"+this.opened);
-    //this.open();
   }
 
   valueClicked($event, axis: string) {
     axis = axis ? axis.trim() : '';
-    let filterValue = $event.currentTarget.textContent.split(':')[0];
+    let filterValue = $event.currentTarget.textContent.split(':')[ 0 ];
     filterValue = filterValue ? filterValue.trim() : '';
     let value = {value: filterValue, axis: axis};
     this.badgeClicked.emit(value);
   }
 
+  clearFilter(axis) {
+  }
 }
-
 
 // WEBPACK FOOTER //
 // ./src/components/distribution/distribution.ts
