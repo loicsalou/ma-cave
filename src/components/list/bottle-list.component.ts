690690bdd5356a1427f31e6152b9bc1ad1125cf9
Loic Salou <business.salou@gmail.com>
2017-05-22 23:29:30 +0200

    mumuse avec les sliding items et le panier + styles. Fun !

--- src/components/list/bottle-list.component.ts
+++ src/components/list/bottle-list.component.ts
@@ -1,8 +1,8 @@
-import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {BottleService} from "../bottle/bottle-firebase.service";
 import {Bottle} from "../bottle/bottle";
 import {ListBottleEvent} from "./bottle-list-event";
-import {Observable} from "rxjs/Observable";
+import {ItemSliding, ToastController} from "ionic-angular";
 
 @Component({
              selector: 'bottle-list',
@@ -10,14 +10,16 @@ import {Observable} from "rxjs/Observable";
              styleUrls: [ '/bottle-list.component.scss' ]
            })
 export class BottleListComponent {
-   isFilterPanelShown = false;
+  isFilterPanelShown = false;
   @Input()
   bottles: Bottle[];
 
   @Output()
   showDetail: EventEmitter<ListBottleEvent> = new EventEmitter();
 
-  constructor(private bottlesService: BottleService) {
+  private basket: Bottle[] = [];
+
+  constructor(private bottlesService: BottleService, private toastCtrl: ToastController) {
   }
 
   filter() {
@@ -33,5 +35,32 @@ export class BottleListComponent {
 
   color(bottle: Bottle) {
     return 'red';
+  }
+
+  isBottleInBasket(bottle: Bottle): boolean {
+    return this.basket.filter(item => item === bottle).length == 1;
+  }
+
+  addToBasketOrRemove(event: Event, slidingItem: ItemSliding, bottle: Bottle) {
+    event.stopPropagation();
+    slidingItem.close();
+    if (this.isBottleInBasket(bottle)) {
+      this.basket=this.basket.filter(btl => btl != bottle);
+      let basketToast = this.toastCtrl.create({
+                                                message: 'la bouteille ' + bottle.nomCru + ' a été retirée du panier',
+                                                duration: 2000,
+                                                position: 'top'
+                                              });
+      basketToast.present();
+    } else {
+      this.basket.push(bottle);
+      let basketToast = this.toastCtrl.create({
+                                                message: 'la bouteille ' + bottle.nomCru + ' a été placée dans le' +
+                                                ' panier',
+                                                duration: 2000,
+                                                position: 'top'
+                                              });
+      basketToast.present();
+    }
   }
 }
