1122c075df0cd5c6f2162f727f7578ba316bcdf7
Loic Salou <business.salou@gmail.com>
2017-07-05 10:26:39 +0200

    - ajout de splashscreen maison + icônes - minimalisation du temps entre l'effacement du splash et l'affichage de l'écran home.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,10 @@ export class MyCaveApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      setTimeout(() => {
+        splashScreen.hide();
+      }, 100);
+
       // this language will be used as a fallback when a translation isn't found in the current language
       translate.setDefaultLang('fr');
 
