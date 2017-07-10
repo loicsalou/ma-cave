7171d34c953079f2ed72d98976f2c581aefa32dc
Loic Salou <business.salou@gmail.com>
2017-07-10 23:42:38 +0200

    - cette nouvelle version fournit les scss et une génération aléatoire de couleur de bouteille par case de chacun des casiers - le couleur de bouteille est transformée en classe CSS définissant la background-image de chaque case des casies

--- src/components/locker/locker.component.ts
+++ src/components/locker/locker.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Locker} from '../../model/locker';
 import * as _ from 'lodash';
+import {Configuration} from '../config/Configuration';
 
 /**
  * Generated class for the LockerComponent component.
@@ -14,6 +15,18 @@ import * as _ from 'lodash';
              styleUrls: [ '/locker.component.scss' ]
            })
 export class LockerComponent implements OnInit {
+  private static COLORS = [ 'rouge',
+    'blanc',
+    'blanc effervescent',
+    'cognac',
+    'demi-sec',
+    'rosé effervescent',
+    'rosé',
+    'vin jaune',
+    'vin blanc muté',
+    'blanc moëlleux',
+    'vin de paille',
+    'blanc liquoreux' ];
 
   @Input()
   locker: Locker;
@@ -27,13 +40,28 @@ export class LockerComponent implements OnInit {
     this.resetComponent();
   }
 
+  label() {
+    let ix = Math.round(Math.random() * 11 );
+    return LockerComponent.COLORS[ ix ];
+  }
+
   private resetComponent() {
-    let cell = new Cell();
-    let cells = new Array<Cell>(this.locker.dimensions.x);
-    _.fill(cells, cell);
-    let row = new Row(cells);
-    this.rows = new Array<Row>(this.locker.dimensions.y);
-    _.fill(this.rows, row);
+    this.rows=[];
+    for (let i=0; i<this.locker.dimensions.y; i++) {
+      this.rows[i]=this.initRow(this.locker.dimensions.x);
+    }
+  }
+
+  private initRow(nbcells: number): Row {
+    let cells: Cell[]=[];
+    for (let i=0; i<nbcells; i++) {
+      cells[i]=new Cell(this.label());
+    }
+    return new Row(cells);
+  }
+
+  private initCell(): Cell {
+    return new Cell(this.label());
   }
 
   ngOnInit(): void {
@@ -58,5 +86,9 @@ class Row {
 }
 
 class Cell {
+  cellClass: string;
 
+  constructor(color: string) {
+    this.cellClass = Configuration.colorsText2Code[ color.toLowerCase() ];
+  }
 }
