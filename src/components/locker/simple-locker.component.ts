af0f2b881dba76d6330c0f4f819fff5ef87e6da8
Loic Salou <business.salou@gmail.com>
2017-08-10 19:58:35 +0200

    - update lockers en cours

--- src/components/locker/simple-locker.component.ts
+++ src/components/locker/simple-locker.component.ts
@@ -1,24 +1,22 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SimpleLocker} from '../../model/simple-locker';
-import {Configuration} from '../config/Configuration';
 import {LockerType} from '../../model/locker';
 import {Bottle, Position} from '../../model/bottle';
 import {NotificationService} from '../../service/notification.service';
+import {Cell, Row, LockerComponent} from './locker.component';
 
 /**
- * Generated class for the LockerComponent component.
+ * Generated class for the SimpleLockerComponent component.
  *
  * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
  * for more info on Angular Components.
  */
 @Component({
              selector: 'locker',
-             templateUrl: './locker.component.html',
+             templateUrl: './simple-locker.component.html',
              styleUrls: [ '/locker.component.scss' ]
            })
-export class LockerComponent implements OnInit {
-
-  public static SIZE_CLASSES = [ 'small', 'medium', 'big', 'huge' ];
+export class SimpleLockerComponent extends LockerComponent implements OnInit {
 
   @Input()
   locker: SimpleLocker;
@@ -26,63 +24,17 @@ export class LockerComponent implements OnInit {
   @Input()
   rack: number = 0;
 
-  @Input()
-  content: Bottle[] = [];
-
-  @Input()
-  highlighted: Bottle[];
-
-  @Input()
-  size: LockerSize;
-
-  @Output()
-  selected: EventEmitter<Cell> = new EventEmitter<Cell>();
-
   rows: Row[];
   private bogusBottles = [];
 
   constructor(private notificationService: NotificationService) {
+    super()
   }
 
   ngOnInit(): void {
     if (this.locker.dimension && !this.rows) {
       this.resetComponent();
     }
-    this.content.forEach(
-      bottle => {
-        if (bottle.positions) {
-          bottle.positions.filter(
-            position => position.inRack(this.locker.id, this.rack)
-          ).forEach(
-            position => this.placeBottle(bottle, position)
-          )
-        }
-      });
-    if (this.bogusBottles.length > 0) {
-      this.notificationService.ask('Position inexistante', this.bogusBottles.length + ' bouteilles sont dans une position' +
-        ' inexistante: les remettre en attente de rangement ?')
-        .subscribe(
-          result => {
-            if (result) {
-              this.notificationService.information('SUPPRESSION DES POSITIONS ERRONEES A IMPLEMENTER')
-              //this.bogusBottles.forEach(btl => btl.positions = btl.positions.filter(
-              //  pos => !(pos.lockerId = position.lockerId && pos.x === position.x && pos.y === position.y)))
-            }
-          })
-    }
-
-  }
-
-  //
-  //ngOnChanges(changes: any) {
-  //  this.resetComponent();
-  //}
-
-  sizeClass(): string {
-    if (this.size) {
-      return LockerComponent.SIZE_CLASSES[ this.size ];
-    }
-    return LockerComponent.SIZE_CLASSES[ LockerSize.medium ];
   }
 
   isShifted(): boolean {
@@ -107,10 +59,32 @@ export class LockerComponent implements OnInit {
     }
   }
 
-  private resetComponent() {
+  public resetComponent() {
     this.rows = [];
     for (let i = 0; i < this.locker.dimension.y; i++) {
       this.rows[ i ] = this.initRow(this.locker.dimension.x, i);
+    }
+    this.content.forEach(
+      bottle => {
+        if (bottle.positions) {
+          bottle.positions.filter(
+            position => position.inRack(this.locker.id, this.rack)
+          ).forEach(
+            position => this.placeBottle(bottle, position)
+          )
+        }
+      });
+    if (this.bogusBottles.length > 0) {
+      this.notificationService.ask('Position inexistante', this.bogusBottles.length + ' bouteilles sont dans une position' +
+        ' inexistante: les remettre en attente de rangement ?')
+        .subscribe(
+          result => {
+            if (result) {
+              this.notificationService.information('SUPPRESSION DES POSITIONS ERRONEES A IMPLEMENTER')
+              //this.bogusBottles.forEach(btl => btl.positions = btl.positions.filter(
+              //  pos => !(pos.lockerId = position.lockerId && pos.x === position.x && pos.y === position.y)))
+            }
+          })
     }
   }
 
@@ -127,7 +101,7 @@ export class LockerComponent implements OnInit {
 
   public placeBottle(bottle: Bottle, position: Position) {
     this.bogusBottles = [];
-    if (this.rows.length < position.y || this.rows[ position.y ].cells.length < position.y) {
+    if (this.rows.length < position.y || this.rows[ position.y ].cells.length < position.x) {
       this.bogusBottles.push(bottle);
     } else {
       let targetCell = this.rows[ position.y ].cells[ position.x ];
@@ -142,77 +116,4 @@ export class LockerComponent implements OnInit {
     }
     return this.highlighted.find(btl => btl.id === bottle.id) !== undefined;
   }
-}
-
-class Row {
-
-  private id: string;
-  index: number;
-  cells: Cell[];
-
-  //constructor(cells: Cell[], id: string, rowIndex: number) {
-  constructor(cells: Cell[], rowIndex: number) {
-    this.cells = cells;
-    //this.id = id;
-    this.index = rowIndex;
-  }
-
-}
-
-export class Cell {
-  bottle: Bottle;
-  cellClass: string;
-  selected = false;
-  position: Position;
-
-  constructor(position: Position) {
-    this.position = position;
-  }
-
-  public isEmpty(): boolean {
-    return this.bottle === undefined;
-  }
-
-  public withdraw(): Bottle {
-    let btl = this.bottle;
-    this.bottle = undefined;
-    this.cellClass = 'empty';
-    if (this.selected) {
-      this.cellClass += ' selected';
-    }
-    return btl;
-  }
-
-  public storeBottle(bottle: Bottle, highlight = false) {
-    if (!bottle) {
-      return;
-    }
-
-    this.bottle = bottle;
-    if (this.isEmpty()) {
-      this.cellClass = 'empty';
-    } else {
-      this.cellClass = Configuration.colorsText2Code[ bottle.label.toLowerCase() ];
-    }
-    if (highlight) {
-      this.cellClass += ' highlighted'
-    }
-  }
-
-  /**
-   * cellule cliquée ==> remonter l'information jusqu'à la page pour qu'une éventuelle bouteille en transit soit
-   * affectée à la cellule cliquée
-   */
-  setSelected(selected: boolean) {
-    this.selected = selected;
-    if (this.selected) {
-      this.cellClass += ' selected';
-    } else {
-      this.cellClass = this.cellClass.replace('selected', '').trim();
-    }
-  }
-}
-
-export enum LockerSize {
-  small, medium, big, huge
 }
